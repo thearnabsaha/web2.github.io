d234ab6f1f662e854adb03720aa596241d3938ce
thearnabsaha <68840187+thearnabsaha@users.noreply.github.com>
2021-09-10 12:55:08 +0530

    fsd

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 let humburger = document.querySelector(".humburger");
+let times = document.querySelector(".times");
 let mobileNav = document.querySelector(".mobileNav");
+
 humburger.addEventListener('click',function(){
     mobileNav.classList.toggle('open')
+})
+humburger.addEventListener('blur',function(){
+    mobileNav.classList.remove('open')
 })
